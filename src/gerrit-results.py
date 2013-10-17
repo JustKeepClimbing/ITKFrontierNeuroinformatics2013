5e145e8d451550c2df8a6fddc42fa59a0249f0ee
Matt McCormick <matt.mccormick@kitware.com>
2013-10-17 00:07:31 -0400

    Add count of Gerrit reviews.

--- src/gerrit-results.py
+++ src/gerrit-results.py
@@ -4,10 +4,23 @@
 
 import json
 
+gerrit_results = {}
+
 with open('../data/gerrit_data.json', 'r') as fp:
     gerrit_data = json.load(fp)
 
 changes = gerrit_data['changes']
 
-### @export "changes"
-print(len(changes))
+gerrit_results['changes'] = len(changes)
+
+reviews = 0
+for change in changes:
+    for patch_set in change['patchSets']:
+        for approval in patch_set.get('approvals', []):
+            if approval['description'] == 'Code Review':
+                reviews += 1
+gerrit_results['reviews'] = reviews
+
+
+with open('gerrit_results.json', 'wb') as fp:
+    json.dump(gerrit_results, fp)
