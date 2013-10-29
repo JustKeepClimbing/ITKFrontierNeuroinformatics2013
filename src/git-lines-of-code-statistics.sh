c3d2ada04629d61d779f70809a970c195aa571cc
Luis Ibanez <luis.ibanez@kitware.com>
2013-10-29 18:03:02 -0400

    Splitted across third party. Improved formatting.
    Computed separatedly the number of lines of code in third party,
    versus non third party, and improved formatting of the output.

--- src/git-lines-of-code-statistics.sh
+++ src/git-lines-of-code-statistics.sh
@@ -11,13 +11,26 @@
 #  Compute the current number of lines of code
 #
 echo "Number of Lines of Code"
-wc -l $(git ls-files | grep -v .md5 ) | tail -n 1
+wc -l $(git ls-files | grep -v .md5 ) | tail -n 1 | cut -f 2 -d' '
+
+#
+#  Compute the current number of third party lines of code
+#
+echo "Number of Third-Party Lines of Code"
+wc -l $(git ls-files | grep -v .md5 | grep ThirdParty) | tail -n 1 | cut -f 2 -d' '
+
+#
+#  Compute the current number of non third party lines of code
+#
+echo "Number of non Third-Party Lines of Code"
+wc -l $(git ls-files | grep -v .md5 | grep -v ThirdParty) | tail -n 1 | cut -f 2 -d' '
 
 
 #
 #  Compute the number of inserted lines
 #
 echo "Number of Inserted Lines of Code"
-itkfirstcommit=`git hash-object -t tree /dev/null`
-git diff --shortstat $itkfirstcommit
+itktreeobjecthash=`git hash-object -t tree /dev/null`
+itkfirstcommit=`git log  --reverse | head -n 1 | cut -d' ' -f 2`
+git diff --shortstat $itkfirstcommit | cut -d' ' -f 5
 
